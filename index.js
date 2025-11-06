d40ebb36d27b29e086cda42b3a8b91fc09f5fe88
farashoghl-star <farashoghl@gmail.com>
2025-11-06 16:40:58 +0330

    Update index.js

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ bot.on("contact", async (ctx) => {
 
   const type = getTypeByScore(st.score) || TYPES[3];
   const gifts =
-    `🎁 هدیه اختصاصی تو آماده‌ی دانلوده:\n${type.giftLink}\n\n` +
+    `🎁 هدیه اختصاصی تو آماده‌ی دانلوده:\n${https://farashoghl.ir/wp-content/uploads/2025/09/razhaye_mamnoe_mashaghl.pdf}\n\n` +
     `📣 به جامعه فراشغل بپیوند:\nhttps://t.me/+RXtqgGDCVvE0MmE0`;
 
   await ctx.reply(`✅ دریافت شد! شماره‌ات ثبت شد: ${phone}`);
